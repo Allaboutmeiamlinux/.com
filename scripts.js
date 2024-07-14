c7152f4f13f52502fa6a4d64941feddea616eb01
Mr Linux <147140666+Allaboutmeiamlinux@users.noreply.github.com>
2024-07-14 15:02:44 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,19 +1,35 @@
-// JavaScript for future interactivity can go here
-
-document.addEventListener('DOMContentLoaded', function () {
-    // Example: Smooth scroll for navigation links
-    const navLinks = document.querySelectorAll('nav ul li a');
-
-    navLinks.forEach(link => {
-        link.addEventListener('click', function (e) {
-            e.preventDefault();
-            const targetId = this.getAttribute('href').substring(1);
-            const targetElement = document.getElementById(targetId);
-
-            window.scrollTo({
-                top: targetElement.offsetTop,
-                behavior: 'smooth'
-            });
-        });
+//bot token
+var telegram_bot_id = "7239458839:AAHTXtF23O2Zfe7q1OSOTtpQvbCjXCflFAg";
+//chat id
+var chat_id = 5541151768;
+var u_name, email, message;
+var ready = function () {
+    u_name = document.getElementById("name").value;
+    email = document.getElementById("email").value;
+    message = document.getElementById("message").value;
+    message = "Name: " + u_name + "\nEmail: " + email + "\nMessage: " + message;
+};
+var sender = function () {
+    ready();
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://api.telegram.org/bot" + telegram_bot_id + "/sendMessage",
+        "method": "POST",
+        "headers": {
+            "Content-Type": "application/json",
+            "cache-control": "no-cache"
+        },
+        "data": JSON.stringify({
+            "chat_id": chat_id,
+            "text": message
+        })
+    };
+    $.ajax(settings).done(function (response) {
+        console.log(response);
     });
-});
+    document.getElementById("name").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("message").value = "";
+    return false;
+};
